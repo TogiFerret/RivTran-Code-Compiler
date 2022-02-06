48ca0e4a2aa88acf2c4c1d6833bdd1af89641cfd
Hudson Reich <>
2022-02-06 22:32:36 +0000

    test

--- RivTranCompiler/Supplement/supplement.js
+++ RivTranCompiler/Supplement/supplement.js
@@ -1,14 +1,13 @@
 // More freaking things to get other things to work //
 var editor = CodeMirror.fromTextArea(document.getElementById('entercode'), {
-    lineNumbers: true,
-    
+  lineNumbers: true,
 });
 function s_plitLines(t) { return t.split(/\r\n|\r|\n/); }
 function g_o() {
   var code = editor.getValue();
   var listOCode = s_plitLines(code);
   listOCode.forEach(lineOCode => {
-    if (lineOCode.startsWith("//")){
+    if (lineOCode.startsWith("//")) {
     } else {
       eval(c_ompiler(lineOCode));
     }
@@ -18,20 +17,26 @@ function g_o() {
 //
 // Basically, dot things, vars, loops, and functions; THe ability to do that is here
 
-var realConsoleLog = console.log;
-console.log = function () {
-    var message = [].join.call(arguments, " ");
-    // Display the message somewhere... (jQuery example)
-    $(".output").text(message);
-    realConsoleLog.apply(console, arguments);
+console.log = function (message) {
+  // Display the message somewhere... (jQuery example)
+  var console = document.getElementById("output");
+  var text = document.createTextNode(message);
+  var temp = document.getElementsByTagName("template")[0];
+  var clon = temp.content.cloneNode(true);
+  console.appendChild(text);
+  console.appendChild(clon);
 };
+function clearConsole() {
+  var console = document.getElementById("output");
+  console.innerHTML = "";
+}
 function consoleLog(message) {
   console.log(message);
 }
 function createVar(name, value) {
   localStorage.setItem(name, value);
 }
-function getVar(name){
+function getVar(name) {
   return localStorage.getItem(name);
 }
 function loop(count, codeAsString) {
@@ -45,4 +50,23 @@ function createFunction(name, codeAsString) {
 
 function runFunction(name) {
   eval(localStorage.getItem(name));
+}
+function join(str1, str2) {
+  return str1 + str2;
+}
+function add(int1, int2) {
+  // Add int2 and int1
+  return int1 + int2;
+}
+function subtract(int1, int2) {
+  // Subtract int2 from int1
+  return int1 - int2;
+}
+function divide(int1, int2) {
+  // Divide int1 by int2
+  return int1/int2;
+}
+function multiply(int1, int2) {
+  // Multiply int2 by int1
+  return int1*int2;
 }
